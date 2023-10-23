0c8b93f47627b6bf88969b5922b4d70dcf5999b0
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-10-23 18:44:39 +0300

    added minor changes to the menus

--- Assets/Scripts/Building/Building.cs
+++ Assets/Scripts/Building/Building.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class Building : MonoBehaviour, ITouchOnce
 {
+    [SerializeField,Tooltip("referance to the menus")]
+    protected MenuReference _menuReference;
+    
+    [SerializeField, Tooltip("Prefab for the unit to be created")]
+    protected Transform _createdUnit;
+
+    [SerializeField, Tooltip("Manager input tracker")]
+    protected SO_InputTracker _InputTracker;
+
+
+    protected BoxCollider2D _buildingCollider;
+
+   
+
+    private void Start()
+    {
+        _buildingCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnEnable()
+    {
+        _InputTracker.onOneTouch += DoTaskOnTouch;
+    }
+
+    private void OnDisable()
+    {
+        _InputTracker.onHoldTouch -= DoTaskOnTouch;
+    }
+
     public void DoTaskOnTouch()
     {
         if (_buildingCollider.OverlapPoint(_InputTracker.touchPosition))
@@ -20,33 +49,5 @@ public class Building : MonoBehaviour, ITouchOnce
     public void OpenMenu(Transform menu)
     {
         menu.GetComponent<MenuAnimations>().OpenMenu(_buildingCollider);
-    }
-
-    private void OnEnable()
-    {
-        _InputTracker.onOneTouch += DoTaskOnTouch;
-    }
-
-    private void OnDisable()
-    {
-        _InputTracker.onHoldTouch -= DoTaskOnTouch;
-    }
-
-    [SerializeField,Tooltip("referance to the menus")]
-    protected MenuReference _menuReference;
-    
-    [SerializeField, Tooltip("Prefab for the unit to be created")]
-    protected Transform _createdUnit;
-
-    [SerializeField, Tooltip("Manager input tracker")]
-    protected SO_InputTracker _InputTracker;
-
-
-    protected BoxCollider2D _buildingCollider;
-
-
-    private void Start()
-    {
-        _buildingCollider = GetComponent<BoxCollider2D>();
     }
 }
